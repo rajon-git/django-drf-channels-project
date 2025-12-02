b57ce6cde622bc29a4ddfde360d1fcfdd53bd8ed
MD. RAJON AHAMAD <rajon.zhsust15@gmail.com>
2025-12-02 22:47:10 +0600

    channel, server and category model create

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -24,6 +24,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "account",
+    "server"
 ]
 
 MIDDLEWARE = [
@@ -107,3 +109,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.Account"
